1a88bdc8b6313e1a3ea3fe9aeb92da7a0e34a5ac
Luke Parry <dev@lukeparry.uk>
2020-04-09 13:53:45 +0100

    Fix for spinx doc build - add extensions Signed-off-by: Luke Parry <dev@lukeparry.uk>`

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.abspath('../pyccx'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
