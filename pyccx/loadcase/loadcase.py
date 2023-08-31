14e91a14c8c03e7e54e4083c3e36e736b02c6550
Luke Parry <luke.parry@mykolab.ch>
2023-08-31 21:50:49 +0100

    Feature: Loadcase Properties
    Includes properties for setting the timestep (initial, minimum and default)  and auto-incremental state and toggle whether Geometric non-linearity is included. Finally state whether a transient or steady-state analysis is computed.

--- pyccx/loadcase/loadcase.py
+++ pyccx/loadcase/loadcase.py
@@ -36,23 +36,27 @@ class LoadCase:
     specified using :meth:`setLoadCaseType`. Depending on the analysis type the steady-state solution
     may instead be calculated.
     """
-    def __init__(self, loadCaseName, loadCaseType: LoadCaseType = None, resultSets = None):
+    def __init__(self, loadCaseName: str, loadCaseType: LoadCaseType = None, resultSets = None):
 
         self._input = ''
         self._loadcaseName = loadCaseName
         self._loadCaseType = None
-        self.isSteadyState = False
-        self.initialTimeStep = 0.1
-        self.defaultTimeStep = 0.1
-        self.totalTime = 1.0
+        self._isSteadyState = False
+        self._isNonlinear = False
+        self._automaticIncrements = True
+        self._initialTimestep = 0.1
+        self._defaultTimeStep = 0.1
+        self._minTimestep = 1e-6
+        self._maxTimestep = 1.0
+        self._totalTime = 1.0
         self._resultSet = []
         self._boundaryConditions = []
 
         if loadCaseType:
-            if loadCaseType is LoadCaseType:
+            if loadCaseType in LoadCaseType:
                 self._loadCaseType = loadCaseType
             else:
-                raise  ValueError('Loadcase type must valid')
+                raise ValueError('Loadcase type must valid')
 
         if resultSets:
             self.resultSet = resultSets
@@ -96,33 +100,88 @@ class LoadCase:
         self._loadcaseName = loadCaseName
 
     @property
+    def maxTimestep(self) -> float:
+        return self._maxTimestep
+
+    @maxTimestep.setter
+    def maxTimestep(self, timeInc: float):
+        self._maxTimestep = timeInc
+
+    @property
+    def minTimestep(self) -> float:
+        return self._minTimestep
+
+    @minTimestep.setter
+    def minTimestep(self, timeInc: float):
+        self._minTimestep = timeInc
+
+    @property
+    def totalTime(self) -> float:
+        return self._totalTime
+
+    @totalTime.setter
+    def totalTime(self, time: float):
+        self._totalTime = time
+
+    @property
+    def defaultTimestep(self) -> float:
+        return self._defaultTimestep
+
+    @defaultTimestep.setter
+    def defaultTimestep(self, timestep: float):
+        self._defaultTimestep = timestep
+
+    @property
+    def initialTimestep(self):
+        return self._initialTimestep
+
+    @initialTimestep.setter
+    def initialTimestep(self, timeStep: float):
+        self._initialTimestep = timeStep
+
+    @property
     def steadyState(self) -> bool:
         """
         Returns True if the loadcase is a steady-state analysis
         """
-        return self.isSteadyState
+        return self._isSteadyState
 
     @steadyState.setter
     def steadyState(self,state:bool) -> None:
-        self.isSteadyState = state
+        self._isSteadyState = state
 
+    @property
+    def automaticIncrements(self) -> bool:
+        return self._automaticIncrements
 
-    def setTimeStep(self, defaultTimeStep: float = 1.0, initialIimeStep: float = None, totalTime: float = None) -> None:
+    @automaticIncrements.setter
+    def automaticIncrements(self, state : bool):
+        self._automaticIncrements = state
+
+    @property
+    def nonlinear(self):
+        return self._isNonlinear
+
+    @nonlinear.setter
+    def nonlinear(self, state):
+        self._isNonlinear = state
+
+    def setTimeStep(self, defaultTimestep: float = 1.0, initialTimestep: float = None, totalTime: float = None) -> None:
         """
         Set the timestepping values for the loadcase
 
-        :param defaultTimeStep: float: Default timestep to use throughout the loadcase
-        :param initialIimeStep:  float: The initial timestep to use for the increment
+        :param defaultTimestep: float: Default timestep to use throughout the loadcase
+        :param initialIimestep:  float: The initial timestep to use for the increment
         :param totalTime: float: The total time for the loadcase
 
         """
-        self.defaultTimeStep = defaultTimeStep
+        self._defaultTimestep = defaultTimestep
 
-        if initialIimeStep is not None:
-            self.initialTimeStep = initialIimeStep
+        if initialTimestep:
+            self._initialTimestep = initialTimestep
 
-        if totalTime is not None:
-            self.totalTime = totalTime
+        if totalTime:
+            self._totalTime = totalTime
 
     def setLoadCaseType(self, loadCaseType: LoadCaseType) -> None:
         """
@@ -149,70 +208,6 @@ class LoadCase:
         for bcond in self.boundaryConditions:
             bcondStr += bcond.writeInput()
 
-        if False:
-            for bcond in self.boundaryConditions:
-
-                if bcond['type'] == 'film':
-
-                    bcondStr += '*FILM\n'
-                    bfaces = bcond['faces']
-                    for i in len(bfaces):
-                        bcondStr += '{:d},F{:d},{:e},{:e}\n'.format(bfaces[i, 0], bfaces[i, 1], bcond['tsink'], bcond['h'])
-
-                elif bcond['type'] == 'bodyflux':
-
-                    bcondStr += '*DFLUX\n'
-                    bcondStr += '{:s},BF,{:e}\n'.format(bcond['el'], bcond['flux'])  # use element set
-
-                elif bcond['type'] == 'faceflux':
-
-                    bcondStr += '*DFLUX\n'
-                    bfaces = bcond['faces']
-                    for i in range(len(bfaces)):
-                        bcondStr += '{:d},S{:d},{:e}\n'.format(bfaces[i, 0], bfaces[i, 1], bcond['flux'])
-
-                elif bcond['type'] == 'radiation':
-
-                    bcondStr += '*RADIATE\n'
-                    bfaces = bcond['faces']
-                    for i in len(bfaces):
-                        bcondStr += '{:d},F{:d},{:e},{:e}\n'.format(bfaces[i, 0], bfaces[i, 1], bcond['tsink'],
-                                                               bcond['emmisivity'])
-
-                elif bcond['type'] == 'fixed':
-
-                    bcondStr += '*BOUNDARY\n'
-                    nodeset = bcond['nodes']
-                    # 1-3 U, 4-6, rotational DOF, 11 = Temp
-
-                    for i in range(len(bcond['dof'])):
-                        if 'value' in bcond.keys():
-                            bcondStr += '{:s},{:d},,{:e}\n'.format(nodeset, bcond['dof'][i],
-                                                               bcond['value'][i])  # inhomogenous boundary conditions
-                        else:
-                            bcondStr += '{:s},{:d}\n'.format(nodeset, bcond['dof'][i])
-
-                elif bcond['type'] == 'accel':
-
-                    bcondStr += '*DLOAD\n'
-                    bcondStr += '{:s},GRAV,{:.3f}, {:.3f},{:.3f},{:.3f}\n'.format(bcond['el'], bcond['mag'], bcond['dir'][0],
-                                                                           bcond['dir'][1], bcond['dir'][2])
-
-                elif bcond['type'] == 'force':
-
-                    bcondStr += '*CLOAD\n'
-                    nodeset = bcond['nodes']
-
-                    for i in bcond['dof']:
-                        bcondStr += '{:s},{:d}\n'.format(nodeset, i, bcond['mag'])
-
-                elif bcond['type'] == 'pressure':
-
-                    bcondStr += '*DLOAD\n'
-                    bfaces = bcond['faces']
-                    for i in range(len(bfaces)):
-                        bcondStr += '{:d},P{:d},{:e}\n'.format(bfaces[i, 0], bfaces[i, 1], bcond['mag'])
-
         return bcondStr
 
     def writeInput(self) -> str:
@@ -230,11 +225,19 @@ class LoadCase:
         else:
             raise ValueError('Loadcase type ({:s} is not currently supported in PyCCX'.format(self._loadCaseType))
 
-        if self.isSteadyState:
+        if self._isSteadyState:
             outStr += ', STEADY STATE'
 
+        if self._isNonlinear:
+            outStr += ', NLGEOM'
+
+        if self._automaticIncrements:
+            outStr += ', DIRECT'
+
         # Write the timestepping information
-        outStr += '\n{:.3f}, {:.3f}\n'.format(self.initialTimeStep, self.totalTime)
+        outStr += '\n{:.3f}, {:.3f} ,{:.3f} , {:.3f}\n'.format(self._initialTimestep,
+                                                               self._totalTime,
+                                                               self._minTimestep, self._maxTimestep)
 
         # Write the individual boundary conditions associated with this loadcase
         outStr += self.writeBoundaryCondition()
@@ -246,3 +249,4 @@ class LoadCase:
         outStr += '*END STEP\n\n'
 
         return outStr
+
