68ba058505adbfe42eb66a822f617ff36865d4d7
Luke Parry <dev@lukeparry.uk>
2023-09-04 22:04:05 +0100

    mesh.elements - Include map parameter for gmsh-calculix translation
    The element node orders differ for 2nd order elements between gmsh-calculix. A mapping function is provided for these elements.

--- pyccx/mesh/__init__.py
+++ pyccx/mesh/__init__.py
@@ -1,3 +1,3 @@
-from .mesher import MeshingAlgorithm2D, MeshingAlgorithm3D
+from .mesher import MeshingAlgorithm2D, MeshingAlgorithm3D, RecombinationAlgorithm
 from .mesh import *
 from .utils import Ent
--- pyccx/mesh/elements.py
+++ pyccx/mesh/elements.py
@@ -42,52 +42,70 @@ class BaseElementType(ABC):
 
     Data = {
         'NODE':  {'id': 15, 'name': 'Node', 'nodes': 1, 'order': 0, 'family': ElementFamilies.Pnt,
+                  'map': [1],
                   'faces': None, 'elementType': ElementTypes.Node},
         'BEAM2': {'id': 1, 'name': 'B31', 'nodes': 2, 'order': 1, 'family': ElementFamilies.Line,
+                  'map': [1, 2],
                   'faces': [[1,2]], 'elementType': ElementTypes.Line},
         'BEAM3': {'id': 1, 'name': 'B32', 'nodes': 3, 'order': 2, 'family': ElementFamilies.Line,
+                  'map': [1, 2, 3],
                   'faces': [[1,2], [2,3]], 'elementType': ElementTypes.Line},
         'TRI3':  {'id': 2, 'name': 'CPS3', 'nodes': 3, 'order': 1, 'family': ElementFamilies.Tri,
+                  'map': [1, 2, 3],
                   'faces': [[1,2],[2,3],[3,1]], 'elementType': ElementTypes.Planar},
         'TRI6':  {'id': 9, 'name': 'CPS6', 'nodes': 3, 'order': 2, 'family': ElementFamilies.Tri,
+                  'map': [1, 2, 3, 4, 5, 6],
                   'faces': [[1,2],[2,3],[3,1]], 'elementType': ElementTypes.Planar},
         'QUAD4': {'id': 3, 'name': 'CPS4', 'nodes': 4, 'order': 1, 'family': ElementFamilies.Quad,
+                  'map': [1, 2, 3, 4],
                   'faces': [[1, 2], [2, 3], [3, 4], [4,1]], 'elementType': ElementTypes.Planar},
         'QUAD8': {'id': 16, 'name': 'CPS8', 'nodes': 8, 'order': 2, 'family': ElementFamilies.Quad,
+                  'map': [1, 2, 3, 4, 5, 6, 7, 8],
                   'faces': [[1, 2], [2, 3], [3, 4], [4,1]],'elementType': ElementTypes.Planar},
         'SHELL3': {'id': 2, 'name': 'S3', 'nodes': 3, 'order': 1, 'family': ElementFamilies.Tri,
+                   'map': [1, 2, 3],
                    'faces': [[1,2],[2,3],[3,1]], 'elementType': ElementTypes.Shell},
         'SHELL4': {'id': 3, 'name': 'S4', 'nodes': 4, 'order': 1, 'family': ElementFamilies.Quad,
+                   'map': [1, 2, 3, 4],
                    'faces': [[1, 2], [2, 3], [3, 4], [4,1]], 'elementType': ElementTypes.Shell},
         'SHELL8': {'id': 16, 'name': 'S8', 'nodes': 8, 'order': 2, 'family': ElementFamilies.Quad,
+                   'map': [1, 2, 3, 4, 5, 6, 7, 8],
                    'faces': [[1, 8, 4, 7, 3, 6, 2, 5]], 'elementType': ElementTypes.Shell},
         'AX3':   {'id': 2, 'name': 'CAX3', 'nodes': 3, 'order': 1, 'family': ElementFamilies.Tri,
+                  'map': [1, 2, 3],
                   'faces': [[1, 2], [2, 3], [3, 1]], 'elementType': ElementTypes.Axisymmetric },
         'AX4':   {'id': 3, 'name': 'CAX4', 'nodes': 4, 'order': 1, 'family': ElementFamilies.Quad,
                   'faces': [[1, 2], [2, 3], [3, 4], [4,1]],
+                  'map': [1,2,3,4],
                   'elementType': ElementTypes.Axisymmetric
                   },
         'TET4':  {'id': 4, 'name': 'C3D4', 'nodes': 4, 'order': 1, 'family': ElementFamilies.Tet,
                   'faces': [[1, 2, 3], [1, 4, 2], [2, 4, 3], [3, 4, 1]],
+                  'map': [1, 2, 3, 4],
                   'elementType': ElementTypes.Volume},
         'TET10': {'id': 11, 'name': 'C3D10', 'nodes': 10, 'order': 2, 'family': ElementFamilies.Tet,
                   'faces': [[1, 2, 3], [1, 4, 2], [2, 4, 3], [3, 4, 1]],
+                  'map': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                   'elementType': ElementTypes.Volume
                   },
         'HEX8':  {'id': 5, 'name': 'C3D8', 'nodes': 8, 'order': 1, 'family': ElementFamilies.Hex,
+                  'map': [1, 2, 3, 4, 5, 6, 7, 8],
                   'faces': [[1, 2, 3, 4], [5, 8, 7, 6], [1, 5, 6, 2], [2, 6, 7, 3], [3, 7, 8, 4], [4, 8, 5, 1]],
                   'elementType': ElementTypes.Volume
                   },
         'HEX8R': {'id': 5, 'name': 'C3D8R', 'nodes': 8, 'order': 1, 'family': ElementFamilies.Hex,
-                   'faces': [[1, 2, 3, 4], [5, 8, 7, 6], [1, 5, 6, 2], [2, 6, 7, 3], [3, 7, 8, 4], [4, 8, 5, 1]],
-                   'elementType': ElementTypes.Volume
+                  'map': [1, 2, 3, 4, 5, 6, 7, 8],
+                  'faces': [[1, 2, 3, 4], [5, 8, 7, 6], [1, 5, 6, 2], [2, 6, 7, 3], [3, 7, 8, 4], [4, 8, 5, 1]],
+                  'elementType': ElementTypes.Volume
                   },
         'HEX20': {'id': 17, 'name': 'C3D20', 'nodes': 20, 'order': 2, 'family': ElementFamilies.Hex,
+                  'map': [1,2,3,4,5,6,7,8,9,12,14,10,17,19,20,18,11,13,15,16],
                   'faces': [[1, 2, 3, 4], [5, 8, 7, 6], [1, 5, 6, 2], [2, 6, 7, 3], [3, 7, 8, 4], [4, 8, 5, 1]],
                   'elementType': ElementTypes.Volume
                   },
         'WEDGE6': {'id': 6, 'name': 'C3D6', 'nodes': 6, 'order': 1, 'family': ElementFamilies.Prism,
                    'faces': [[1,2,3], [4,5,6], [1,2,5,4], [2,3,6,5], [3,1,4,6]],
+                   'map': [1, 2, 3, 4, 5, 6],
                    'elementType': ElementTypes.Volume
                    },
     }
@@ -116,13 +134,17 @@ class BaseElementType(ABC):
         return cls.Data[cls.Type]['faces']
 
     @classproperty
+    def map(cls) -> List[List[int]]:
+        return cls.Data[cls.Type]['map']
+
+    @classproperty
     def elementType(cls) -> List[List[int]]:
         return cls.Data[cls.Type]['elementType']
 
     @classproperty
     def faceMask(cls) -> List[List[int]]:
 
-        nodeNum = cls.Data[cls.Type]['nodes']
+        nodeNum = cls.Data[cls.Type]['nodes'] #np.max(cls.Data[cls.Type]['faces']) #
         faceIds = cls.Data[cls.Type]['faces']
         mask = np.zeros([len(faceIds), nodeNum])
 
