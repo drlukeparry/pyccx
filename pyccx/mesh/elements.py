292b7aba057b048784d548e7d490d74077d8826b
Luktug Ltd <luke.parry@luktug.co.uk>
2024-11-05 23:31:00 +0000

    Update element library
    Includes 1st and 2nd order axisymmetric elements

--- pyccx/mesh/elements.py
+++ pyccx/mesh/elements.py
@@ -8,25 +8,28 @@ from .utils import classproperty
 
 class ElementFamilies(IntEnum):
     """ Element Family Types"""
-    Pnt     = 1
-    Line    = 2
-    Tri     = 3
-    Quad    = 4
-    Tet     = 5
+    Pnt         = 1
+    Line       = 2
+    Tri          = 3
+    Quad      = 4
+    Tet         = 5
     Pyramid = 6
-    Prism   = 7
-    Hex     = 8
+    Prism     = 7
+    Hex        = 8
 
     @classmethod
     def has_value(cls, value):
         return value in cls._value2member_map_
 
 class ElementTypes(IntEnum):
-    """ Element Family Types"""
+    """
+    The Element Family Types available generally for Calculix and other FEA solvers. This includes common 1D (beam),
+    2D planar and axisymmetric elements and various 3D element formulations (3D continuum and 3D shell elements)
+    """
     Node     = 1
-    Line     = 2
-    Planar   = 3
-    Shell    = 4
+    Line       = 2
+    Planar    = 3
+    Shell      = 4
     Axisymmetric = 5
     Volume   = 6
 
@@ -36,7 +39,11 @@ class ElementTypes(IntEnum):
 
 
 class BaseElementType(ABC):
-
+    """
+    Library of Base Element Types for Calculix, which includes both the element type and the element family and
+    corresponding Calculix elemental name and nodal mappings for the element type when identifying surfaces or edges
+    for application of flux boundary conditions.
+    """
 
     Type = None
 
@@ -53,7 +60,7 @@ class BaseElementType(ABC):
         'TRI3':  {'id': 2, 'name': 'CPS3', 'nodes': 3, 'order': 1, 'family': ElementFamilies.Tri,
                   'map': [1, 2, 3],
                   'faces': [[1,2],[2,3],[3,1]], 'elementType': ElementTypes.Planar},
-        'TRI6':  {'id': 9, 'name': 'CPS6', 'nodes': 3, 'order': 2, 'family': ElementFamilies.Tri,
+        'TRI6':  {'id': 9, 'name': 'CPS6', 'nodes': 6, 'order': 2, 'family': ElementFamilies.Tri,
                   'map': [1, 2, 3, 4, 5, 6],
                   'faces': [[1,2],[2,3],[3,1]], 'elementType': ElementTypes.Planar},
         'QUAD4': {'id': 3, 'name': 'CPS4', 'nodes': 4, 'order': 1, 'family': ElementFamilies.Quad,
@@ -68,17 +75,24 @@ class BaseElementType(ABC):
         'SHELL4': {'id': 3, 'name': 'S4', 'nodes': 4, 'order': 1, 'family': ElementFamilies.Quad,
                    'map': [1, 2, 3, 4],
                    'faces': [[1, 2], [2, 3], [3, 4], [4,1]], 'elementType': ElementTypes.Shell},
+        'SHELL6': {'id': 9, 'name': 'S6', 'nodes': 6, 'order': 2, 'family': ElementFamilies.Tri,
+                 'map': [1, 2, 3, 4, 5, 6],
+                 'faces': [[1, 2], [2, 3], [3, 1]], 'elementType': ElementTypes.Shell},
         'SHELL8': {'id': 16, 'name': 'S8', 'nodes': 8, 'order': 2, 'family': ElementFamilies.Quad,
                    'map': [1, 2, 3, 4, 5, 6, 7, 8],
                    'faces': [[1, 8, 4, 7, 3, 6, 2, 5]], 'elementType': ElementTypes.Shell},
-        'AX3':   {'id': 2, 'name': 'CAX3', 'nodes': 3, 'order': 1, 'family': ElementFamilies.Tri,
-                  'map': [1, 2, 3],
-                  'faces': [[1, 2], [2, 3], [3, 1]], 'elementType': ElementTypes.Axisymmetric },
-        'AX4':   {'id': 3, 'name': 'CAX4', 'nodes': 4, 'order': 1, 'family': ElementFamilies.Quad,
-                  'faces': [[1, 2], [2, 3], [3, 4], [4,1]],
-                  'map': [1,2,3,4],
-                  'elementType': ElementTypes.Axisymmetric
-                  },
+        'AX3':  {'id': 2, 'name': 'CAX3', 'nodes': 3, 'order': 1, 'family': ElementFamilies.Tri,
+                    'map': [1, 2, 3],
+                    'faces': [[1, 2], [2, 3], [3, 1]], 'elementType': ElementTypes.Axisymmetric},
+        'AX6': {'id': 9, 'name': 'CAX6', 'nodes': 6, 'order': 2, 'family': ElementFamilies.Tri,
+                   'map': [1, 2, 3, 4, 5, 6],
+                   'faces': [[1, 2], [2, 3], [3, 1]], 'elementType': ElementTypes.Axisymmetric},
+        'AX4':  {'id': 3, 'name': 'CAX4', 'nodes': 4, 'order': 1, 'family': ElementFamilies.Quad,
+                     'faces': [[1, 2], [2, 3], [3, 4], [4,1]],
+                     'map': [1,2,3,4], 'elementType': ElementTypes.Axisymmetric},
+        'AX8': {'id': 16, 'name': 'S8', 'nodes': 8, 'order': 2, 'family': ElementFamilies.Quad,
+                   'map': [1, 2, 3, 4, 5, 6, 7, 8],
+                   'faces': [[1, 8, 4, 7, 3, 6, 2, 5]], 'elementType': ElementTypes.Axisymmetric},
         'TET4':  {'id': 4, 'name': 'C3D4', 'nodes': 4, 'order': 1, 'family': ElementFamilies.Tet,
                   'faces': [[1, 2, 3], [1, 4, 2], [2, 4, 3], [3, 4, 1]],
                   'map': [1, 2, 3, 4],
@@ -86,28 +100,23 @@ class BaseElementType(ABC):
         'TET10': {'id': 11, 'name': 'C3D10', 'nodes': 10, 'order': 2, 'family': ElementFamilies.Tet,
                   'faces': [[1, 2, 3], [1, 4, 2], [2, 4, 3], [3, 4, 1]],
                   'map': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-                  'elementType': ElementTypes.Volume
-                  },
+                  'elementType': ElementTypes.Volume},
         'HEX8':  {'id': 5, 'name': 'C3D8', 'nodes': 8, 'order': 1, 'family': ElementFamilies.Hex,
                   'map': [1, 2, 3, 4, 5, 6, 7, 8],
                   'faces': [[1, 2, 3, 4], [5, 8, 7, 6], [1, 5, 6, 2], [2, 6, 7, 3], [3, 7, 8, 4], [4, 8, 5, 1]],
-                  'elementType': ElementTypes.Volume
-                  },
+                  'elementType': ElementTypes.Volume},
         'HEX8R': {'id': 5, 'name': 'C3D8R', 'nodes': 8, 'order': 1, 'family': ElementFamilies.Hex,
                   'map': [1, 2, 3, 4, 5, 6, 7, 8],
                   'faces': [[1, 2, 3, 4], [5, 8, 7, 6], [1, 5, 6, 2], [2, 6, 7, 3], [3, 7, 8, 4], [4, 8, 5, 1]],
-                  'elementType': ElementTypes.Volume
-                  },
+                  'elementType': ElementTypes.Volume},
         'HEX20': {'id': 17, 'name': 'C3D20', 'nodes': 20, 'order': 2, 'family': ElementFamilies.Hex,
                   'map': [1,2,3,4,5,6,7,8,9,12,14,10,17,19,20,18,11,13,15,16],
                   'faces': [[1, 2, 3, 4], [5, 8, 7, 6], [1, 5, 6, 2], [2, 6, 7, 3], [3, 7, 8, 4], [4, 8, 5, 1]],
-                  'elementType': ElementTypes.Volume
-                  },
+                  'elementType': ElementTypes.Volume},
         'WEDGE6': {'id': 6, 'name': 'C3D6', 'nodes': 6, 'order': 1, 'family': ElementFamilies.Prism,
                    'faces': [[1,2,3], [4,5,6], [1,2,5,4], [2,3,6,5], [3,1,4,6]],
                    'map': [1, 2, 3, 4, 5, 6],
-                   'elementType': ElementTypes.Volume
-                   },
+                   'elementType': ElementTypes.Volume},
     }
 
     def __init__(self):
@@ -207,7 +216,7 @@ class TRI3(BaseElementType):
     Type = 'TRI3'
 
 class TRI6(BaseElementType):
-    """ 1st order Tri Planar Stress Element (CPS4) """
+    """ 2nd order Tri Planar Stress Element (CPS4) """
     Type = 'TRI6'
 
 class QUAD4(BaseElementType):
@@ -226,6 +235,10 @@ class SHELL4(BaseElementType):
     """ 1st order Quad Shell Element (S4) """
     Type = 'SHELL4'
 
+class SHELL6(BaseElementType):
+    """ 2nd order Tri Shell Element (S6) """
+    Type = 'SHELL6'
+
 class SHELL8(BaseElementType):
     """ 2nd order Quad Shell Element (S8) """
     Type = 'SHELL8'
@@ -238,12 +251,20 @@ class AX4(BaseElementType):
     """ 1st order Axisymmetric Quad Element (CAX4) """
     Type = 'AX4'
 
+class AX6(BaseElementType):
+    """ 2nd order Axisymmetric Tri Element (CAX4) """
+    Type = 'AX6'
+
+class AX8(BaseElementType):
+    """ 2nd order Axisymmetric Quad Element (CAX4) """
+    Type = 'AX8'
+
 def elementTypes():
     """
     Returns the list of available element types available
     :return:
     """
-    availableElementTypes = [NODE, BEAM2, BEAM3, TRI3, TRI6, TET4, TET10, HEX8, HEX20, HEX8R, SHELL3, SHELL4, SHELL8, WEDGE6]
+    availableElementTypes = [NODE, BEAM2, BEAM3, TRI3, TRI6, TET4, TET10, HEX8, HEX20, HEX8R, AX3, AX4, AX6, AX8, SHELL3, SHELL4, SHELL8, WEDGE6]
 
     return availableElementTypes
 
