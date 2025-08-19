46ec928e576ca705d8d1ce4b009ee5c768e7ae38
Luktug Ltd. <luke.parry@luktug.co.uk>
2025-08-19 23:49:12 +0100

    Update config for ReadTheDocs

--- docs/conf.py
+++ docs/conf.py
@@ -1,13 +1,15 @@
 import sys, os
 import mock
 
+sys.path.insert(0, os.path.abspath('../pyccx'))
+sys.path.insert(0, os.path.abspath('..'))
+
 from pyccx.version import __version__
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration -----------------------------------------------------
 
